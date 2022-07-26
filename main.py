62bba279e6f6f4afcf10ea22f8544b12dbabed79
Andres Villasmil <aevillas@uncg.edu>
2022-07-26 21:49:40 +0000

    Updated API Key

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#This is a thing I'm adding to make a commit
+API_KEY = os.environ['API_KEY']
+print(API_KEY)
 
 Base = declarative_base()
 
@@ -44,7 +45,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'week4-project/static/images'
 app.config['SECRET_KEY'] = 'fec93d1b1cb7926beb25960608b25818'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-map_client = googlemaps.Client('[[API_KEY]]')
+map_client = googlemaps.Client(API_KEY)
 Session = sessionmaker(engine)
 
 user_data = None
