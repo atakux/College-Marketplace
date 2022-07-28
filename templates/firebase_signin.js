b983c3a846e9e03652e02cf7ca59b950412e9f35
LizardRain <ffsarcd@gmail.com>
2022-07-28 13:45:02 -0400

    second commit

--- templates/firebase_signin.js
+++ templates/firebase_signin.js
@@ -1,16 +1,19 @@
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js";
 
+
 const auth = getAuth();
+
+
 console.log("in sign in");
 
 const loginBtn = document.querySelector('#btn');
-    signupBtn.addEventListener('click', e => {
+    loginBtn.addEventListener('click', e => {
     e.preventDefault();
 
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
 
-    auth.signInWithEmailAndPassword(email, password)
+    signInWithEmailAndPassword(auth, email, password)
     .then(cred => {
       console.log('Logged in user!');
       window.location.href = '/signup.html';
