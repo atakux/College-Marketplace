5322272bdaa269f813f41b65d3e0b9af272b2cf6
LizardRain <ffsarcd@gmail.com>
2022-07-27 20:44:32 -0400

    second commit

---
+++ templates/modules/firebase_signin.js
@@ -0,0 +1,23 @@
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js";
+
+const auth = getAuth();
+console.log("in sign in");
+
+const loginBtn = document.querySelector('#btn');
+    signupBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(cred => {
+      console.log('Logged in user!');
+      window.location.href = '/signup.html';
+    })
+    .catch(error => {
+      console.log(error.message);
+    })
+});
+
+console.log("in sign in");
