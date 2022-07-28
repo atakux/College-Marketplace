ce95c6425927a9290e59f04725c9c327e203e3dc
LizardRain <ffsarcd@gmail.com>
2022-07-28 14:09:20 -0400

    second commit

---
+++ templates/firebase_signup.js
@@ -0,0 +1,31 @@
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js";
+import { collection, doc, setDoc, getFirestore } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js";
+
+const auth = getAuth();
+const db = getFirestore();
+const usersRef = collection(db, "user-info");
+
+console.log("in sign up");
+const signupBtn = document.querySelector('#btn');
+    signupBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    const username = document.querySelector("#userName").value;
+    const new_email = document.querySelector('#email').value;
+    const new_password = document.querySelector('#password').value;
+    const zip = document.querySelector('#address').value;
+
+    createUserWithEmailAndPassword(auth, new_email, new_password).then(() => {
+    console.log('User signed up!');
+    console.log(auth.currentUser.uid)
+    setDoc(doc(usersRef, auth.currentUser.uid), {
+        email: String(new_email),
+        id: auth.currentUser.uid,
+        name: username,
+        zip_cope: zip
+    });
+
+  });
+});
+
+console.log("in sign up");
