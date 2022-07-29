0af7cf98e813a4d5f9a9cf5930f6bec0a585a523
LizardRain <ffsarcd@gmail.com>
2022-07-29 13:55:34 -0400

    post item commit

--- templates/firebase_postitem.js
+++ templates/firebase_postitem.js
@@ -1,10 +1,12 @@
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js";
 import { collection, doc, setDoc, getFirestore, getDoc, FieldValue } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js";
 
+const auth = getAuth();
 const db = getFirestore();
 const itemsRef = collection(db, "item-info");
 const pksRef = doc(db, "pks", "item-info");
 const docSnap = await getDoc(pksRef);
-const increment = FieldValue.increment(1);
+//const increment = db.FieldValue.increment(1);
 
 console.log("in item add");
 const addItemBtn = document.querySelector('#btn');
@@ -15,16 +17,16 @@ const addItemBtn = document.querySelector('#btn');
     const new_price = document.querySelector('#price').value;
     const new_desc = document.querySelector('#itemDesc').value;
 
-    setDoc(doc(itemsRef, Number(docSnap.data().id)), {
+    setDoc(doc(itemsRef, String(docSnap.data().id)), {
         name: String(new_name),
-        id: Number(docSnap.data().id),
+        id: Number(docSnap.data().id + 1),
         price: new_price,
         description: new_desc,
         seller_id: auth.currentUser.uid,
         active: 1
     });
 
-    pksRef.update({ id: increment});
+    pksRef.update("id", FieldValue.increment(1));
 });
 
 console.log("in item add");
