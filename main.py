9f3138e9a5ed8a4bbd01eae1a218aaf279cf4ff9
Andres Villasmil <aevillas@uncg.edu>
2022-08-03 06:29:17 +0000

    Added Review Page, Reviews and Stars

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ if not inspector.has_table("user"):
         "`user_email` TEXT NOT NULL,"
         "`user_zip` TEXT NOT NULL,"
         "`user_password` TEXT NOT NULL,"
-        "`user_status` INTEGER NOT NULL DEFAULT 0"
+        "`user_status` INTEGER NOT NULL DEFAULT 0,"
+        "`user_score` INTEGER NOT NULL DEFAULT 0"
         ")")
 
 if not inspector.has_table("item"):
@@ -314,41 +315,8 @@ def get_item(id: int):
         return render_template('itempage.html', item=item_data, seller=seller_data, user_zip=user_place_id,
                                seller_zip=seller_place_id, user_id=user_data['user_id'], distance=distance_miles, API_KEY=API_KEY)
     else:
-        #session['next'] = url_for('get_item', id=id)
+        flash("You must Login to view this page")
         return redirect(url_for('login'))
-
-
-@app.route('/send_report/<int:id>')
-def send_report(id: int):
-    user_data = get_login_user_data()
-    if user_data is not None:
-        if user_data['user_status'] == 1:
-            reported_data = get_user_data_by_id(id)
-            smtp = smtplib.SMTP('smtp.gmail.com', 587)
-            smtp.ehlo()
-            smtp.starttls()
-            msg = MIMEMultipart()
-            msg['Subject'] = "User {} has been reported!!".format(reported_data["user_name"])
-            # Login with your email and password
-            smtp.login(EMAIL_ADDRESS, EMAIL_APP_PASSWORD)
-
-            # message to be sent
-            txt = "{} has reported {}".format(user_data, reported_data)
-            msg.attach(MIMEText(txt))
-
-            # sending the mail
-            smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg.as_string())
-
-            # Finally, don't forget to close the connection
-            smtp.quit()
-            flash("Report Sent!")
-            return redirect(url_for('home'))
-        elif user_data['user_status'] == 0:
-            flash("You must verify your email to do this action")
-            return redirect(url_for('home'))
-    else:
-        return redirect(url_for('login'))
-
 
 @app.route('/review/<int:id>', methods=["GET", "POST"])
 def submit_review(id: int):
@@ -366,10 +334,11 @@ def submit_review(id: int):
                                "VALUES (?, ?, ?, ?);", (int(score), rev_content, id, user_data["user_id"]))
             return render_template('review.html')
         elif user_data['user_status'] == 0:
-            flash('You must verify your email to do this action')
+            flash('You must verify your email to do this action!')
             return redirect(url_for('home'))
     else:
         session['next'] = url_for('submit_review', id=id)
+        flash("You must Login to view this page")
         return redirect(url_for('login'))
         
 @app.route('/sell', methods=['POST', 'GET'])
@@ -422,10 +391,11 @@ def sell_item():
                     return redirect(url_for('get_item', id=id_num))
             return render_template('post_item.html')
         elif user_data['user_status'] == 0:
-            flash('You must verify your email to do this action')
+            flash('You must verify your email to do this action!')
             return redirect(url_for('home'))
     else:
         session['next'] = url_for('sell_item')
+        flash("You must Login to view this page")
         return redirect(url_for('login'))
 
 
@@ -456,10 +426,11 @@ def manage():
 
             return render_template('manage_item.html', item_list=data)
         elif user_data['user_status'] == 0:
-            flash('You must verify your email to do this action')
+            flash('You must verify your email to do this action!')
             return redirect(url_for('home'))
     else:
         session['next'] = url_for('manage')
+        flash("You must Login to view this page")
         return redirect(url_for('login'))
 
 
@@ -474,7 +445,7 @@ def user_profile(id: int):
         
         with sqlal_session_gen.begin() as generated_session:
             review_results = generated_session.execute(text("SELECT * FROM review JOIN user ON user.user_id=review.user_id "
-                                                "WHERE seller_id={}".format(id)))
+                                                "WHERE seller_id={} ORDER BY review_id DESC".format(id)))
             for review in review_results:
                 review_data.append(dict(review))
 
@@ -482,20 +453,37 @@ def user_profile(id: int):
         
         with sqlal_session_gen.begin() as generated_session:
             item_results = generated_session.execute(text("SELECT * FROM item"
-                                                " WHERE seller_id={}".format(id)))
+                                                " WHERE seller_id={} ORDER BY item_id DESC".format(id)))
             for item in item_results:
                 item_data.append(dict(item))
         
         if request.method == 'POST':
-                score = request.form.get('score', 'default score')
-                rev_content = request.form.get('reviewContent', 'default content')
-                engine.execute("INSERT INTO review (review_score, review_text, seller_id, user_id) "
-                               "VALUES (?, ?, ?, ?);", (int(score), rev_content, id, user_data["user_id"]))
+            score = request.form.get('score', 'default score')
+            rev_content = request.form.get('reviewContent', 'default content')
+            engine.execute("INSERT INTO review (review_score, review_text, seller_id, user_id) "
+                            "VALUES (?, ?, ?, ?);", (int(score), rev_content, id, user_data["user_id"]))
+            
+            #Set Score
+            reviewed_user = get_user_data_by_id(id)
+            new_score = 0
+            review_count = 0
+            with sqlal_session_gen.begin() as generated_session:
+                user_reviews = generated_session.execute(text("SELECT * FROM review WHERE seller_id={}".format(id)))
+                for review in user_reviews:
+                    r = dict(review)
+                    review_count += 1
+                    new_score += r['review_score']
+                    
+            new_score = round(new_score/review_count, 1)
+            engine.execute(f"UPDATE user SET user_score='{new_score}' WHERE user_id={id}")
 
+
+            return redirect(url_for('user_profile', id=id))
         return render_template('user.html', item_list=item_data, review_list=review_data, 
         seller_data=seller_data, user_data=user_data)
     else:
         session['next'] = url_for('sell_item')
+        flash("You must Login to view this page")
         return redirect(url_for('login'))
 
 
@@ -514,7 +502,7 @@ def send_report(id: int):
             smtp.login(EMAIL_ADDRESS, EMAIL_APP_PASSWORD)
 
             # message to be sent
-            txt = "{} has reported {}".format(user_data, reported_data)
+            txt = "User {} (Email: {}, ID: {}) has reported User {} (Email: {}, ID {}). Please investigate.".format(user_data['user_name'], user_data['user_email'], user_data['user_id'], reported_data['user_name'], reported_data['user_email'], reported_data['user_id'])
             msg.attach(MIMEText(txt))
 
             # sending the mail
@@ -525,9 +513,10 @@ def send_report(id: int):
             flash("Report Sent!")
             return redirect(url_for('home'))
         elif user_data['user_status'] == 0:
-            flash("You must verify your email to do this action")
+            flash("You must verify your email to do this action!")
             return redirect(url_for('home'))
     else:
+        flash("You must Login to view this page")
         return redirect(url_for('login'))
 
 
@@ -545,67 +534,72 @@ def message(id: int=0):
     list_of_messages = []
     user_data = get_login_user_data()
     if user_data is not None:
-        #Left Signature
-        with sqlal_session_gen.begin() as generated_session:
-            users_current_sent_to_results = generated_session.execute(text("SELECT receiver_id AS important_id, "
-                "max(message_id) AS message_num, message_content, user_name FROM (SELECT receiver_id, message_id, sender_id,"
-                " message_content, user_name FROM message JOIN user ON receiver_id=user_id WHERE sender_id={}) z "
-                " GROUP BY important_id "
-                "ORDER BY message_id desc".format(user_data["user_id"])))
-            for ucstr in users_current_sent_to_results:
-                users_current_sent_to_data.append(dict(ucstr))
-
-        with sqlal_session_gen.begin() as generated_session:
-            users_current_got_from_results = generated_session.execute(text("SELECT sender_id AS important_id, "
-                "max(message_id) AS message_num, message_content, user_name FROM (SELECT receiver_id, message_id, sender_id,"
-                " message_content, user_name FROM message JOIN user ON sender_id=user_id WHERE receiver_id={}) z"
-                " GROUP BY important_id "
-                "ORDER BY message_id desc".format(user_data["user_id"])))
-            for ucgfr in users_current_got_from_results:
-                users_current_got_from_data.append(dict(ucgfr))
-        
-
-        users_current_commed_with.extend(users_current_sent_to_data)
-        users_current_commed_with.extend(users_current_got_from_data)
-        data = sorted(users_current_commed_with, key=lambda x:x['message_num'], reverse=True)
-        data_dict = {}
-        dupe_indices = []
-        for index in range(len(data)):
-            if data[index]['important_id'] in data_dict.keys():
-                dupe_indices.append(index)
-            else:
-                data_dict[data[index]['important_id']] = data[index]['message_num']
-        dupe_list = reversed(dupe_indices)
-        for dupe in dupe_list:
-            data.pop(dupe)
-        
-        #Get Current Data
-        if id != 0:
-            if user_data["user_id"] == id:
-                return redirect('/home')
-
-            #Get other user data
+        if user_data['user_status'] == 1:
+            #Left Signature
             with sqlal_session_gen.begin() as generated_session:
-                other_user_results = generated_session.execute(text('select * from user where user_id={}'.format(id)))
-                for oud in other_user_results:
-                    other_user_data = dict(oud)
+                users_current_sent_to_results = generated_session.execute(text("SELECT receiver_id AS important_id, "
+                    "max(message_id) AS message_num, message_content, user_name FROM (SELECT receiver_id, message_id, sender_id,"
+                    " message_content, user_name FROM message JOIN user ON receiver_id=user_id WHERE sender_id={}) z "
+                    " GROUP BY important_id "
+                    "ORDER BY message_id desc".format(user_data["user_id"])))
+                for ucstr in users_current_sent_to_results:
+                    users_current_sent_to_data.append(dict(ucstr))
 
-            #Get List of Messages
             with sqlal_session_gen.begin() as generated_session:
-                message_results = generated_session.execute(text("select * from message where "
-                "(sender_id={} and receiver_id={}) or (sender_id={} and receiver_id={})".format(user_data['user_id'],
-                other_user_data['user_id'], other_user_data['user_id'], user_data['user_id'])))
-                for mr in message_results:
-                    list_of_messages.append(dict(mr))
+                users_current_got_from_results = generated_session.execute(text("SELECT sender_id AS important_id, "
+                    "max(message_id) AS message_num, message_content, user_name FROM (SELECT receiver_id, message_id, sender_id,"
+                    " message_content, user_name FROM message JOIN user ON sender_id=user_id WHERE receiver_id={}) z"
+                    " GROUP BY important_id "
+                    "ORDER BY message_id desc".format(user_data["user_id"])))
+                for ucgfr in users_current_got_from_results:
+                    users_current_got_from_data.append(dict(ucgfr))
+            
 
-            #Post
-            if request.method == 'POST':
-                msg_content = request.form.get('messageContent', 'default content')
-                engine.execute("INSERT INTO message (sender_id, receiver_id, message_content) VALUES (?, ?, ?);",
-                (user_data["user_id"], id, msg_content))
-                return redirect(url_for('message', id=id))
-        return render_template('message.html', sender=user_data, receiver=other_user_data, message_list=list_of_messages, users_list=data)
+            users_current_commed_with.extend(users_current_sent_to_data)
+            users_current_commed_with.extend(users_current_got_from_data)
+            data = sorted(users_current_commed_with, key=lambda x:x['message_num'], reverse=True)
+            data_dict = {}
+            dupe_indices = []
+            for index in range(len(data)):
+                if data[index]['important_id'] in data_dict.keys():
+                    dupe_indices.append(index)
+                else:
+                    data_dict[data[index]['important_id']] = data[index]['message_num']
+            dupe_list = reversed(dupe_indices)
+            for dupe in dupe_list:
+                data.pop(dupe)
+            
+            #Get Current Data
+            if id != 0:
+                if user_data["user_id"] == id:
+                    return redirect('/home')
+
+                #Get other user data
+                with sqlal_session_gen.begin() as generated_session:
+                    other_user_results = generated_session.execute(text('select * from user where user_id={}'.format(id)))
+                    for oud in other_user_results:
+                        other_user_data = dict(oud)
+
+                #Get List of Messages
+                with sqlal_session_gen.begin() as generated_session:
+                    message_results = generated_session.execute(text("select * from message where "
+                    "(sender_id={} and receiver_id={}) or (sender_id={} and receiver_id={})".format(user_data['user_id'],
+                    other_user_data['user_id'], other_user_data['user_id'], user_data['user_id'])))
+                    for mr in message_results:
+                        list_of_messages.append(dict(mr))
+
+                #Post
+                if request.method == 'POST':
+                    msg_content = request.form.get('messageContent', 'default content')
+                    engine.execute("INSERT INTO message (sender_id, receiver_id, message_content) VALUES (?, ?, ?);",
+                    (user_data["user_id"], id, msg_content))
+                    return redirect(url_for('message', id=id))
+            return render_template('message.html', sender=user_data, receiver=other_user_data, message_list=list_of_messages, users_list=data)
+        elif user_data['user_status'] == 0:
+            flash("You must verify your email to do this action!")
+            return redirect(url_for('home'))
     else:
+        flash("You must Login to view this page")
         return redirect('/login')
 
 @app.route('/chat/update/<sender_id>', methods=['GET'])
