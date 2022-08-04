3698e6088374cbaab5b0dd19fdb5327a9451d2fd
Andres Villasmil <aevillas@uncg.edu>
2022-08-04 23:16:35 +0000

    Updated Database

--- main.py
+++ main.py
@@ -160,10 +160,12 @@ def sign_up():
             dup_email = False
             dup_user_name = False
             with sqlal_session_gen.begin() as generated_session:
-                user_results = generated_session.execute(text('SELECT * FROM user_database WHERE user_email="{}";'.format(str(email))))
+                #user_results = generated_session.execute(text('SELECT * FROM user_database WHERE user_email="{}";'.format(str(email))))
+                user_results = generated_session.execute('SELECT * FROM user_database WHERE user_email="{}";', [email])
                 for ur in user_results:
                     dup_email = True
-                user_results = generated_session.execute(text('SELECT * FROM user_database WHERE user_name="{}";'.format(str(user_name))))
+                #user_results = generated_session.execute(text('SELECT * FROM user_database WHERE user_name="{}";'.format(str(user_name))))
+                user_results = generated_session.execute('SELECT * FROM user_database WHERE user_name="{}";', [user_name])
                 for ur in user_results:
                     dup_user_name = True
 
